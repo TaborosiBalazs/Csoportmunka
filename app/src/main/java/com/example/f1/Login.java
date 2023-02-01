8d403fa6022d48d68ff81ed9fde9477e0acad74d
sz1tabist <sz1tabist@vasvari.org>
2023-02-01 08:51:49 +0100

    Register, login megvan

--- app/src/main/java/com/example/f1/Login.java
+++ app/src/main/java/com/example/f1/Login.java
@@ -1,29 +1,24 @@
 package com.example.f1;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Login extends AppCompatActivity {
 
-    Button loginButton;
     EditText usernameEditText, passwordEditText;
-
+    Button loginButton;
+    LoginUser loginUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        loginUser = new LoginUser();
         usernameEditText = findViewById(R.id.editTextTextPersonName);
-        loginButton = findViewById(R.id.button3);
         passwordEditText = findViewById(R.id.editTextTextPassword);
+        loginButton = findViewById(R.id.button3);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,24 +26,10 @@ public class Login extends AppCompatActivity {
                 String username, password;
                 username = String.valueOf(usernameEditText.getText());
                 password = String.valueOf(passwordEditText.getText());
-
-                if(!username.equals("") && !password.equals(""))
-                {
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            String[] field = new String[2];
-                            field[0] = "username";
-                            field[1] = "password";
-                            String[] data = new String[2];
-                            data[0] = username;
-                            data[1] = password;
-                            PutData putData = new PutData("http://10.0.14.10/f1/signUp.php","POST", field, data);
-                        }
-                    });
-                }
+                User user = new User("",username,password);
+                loginUser.putDataToDB(user,getApplicationContext());
             }
         });
     }
+
 }
